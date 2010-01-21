5fed8ac28d9833963293f5608273843c5797773c
kisieel <kisieel@gmail.com>
2010-01-22 00:34:25 +0100

    ground reflective sensors

---
+++ src/ground.cpp
@@ -0,0 +1,15 @@
+#include "sumo.h"
+
+void ground_init()
+{
+	//set data direction port
+	clrb(GROUND_DDR, GROUND_0_PIN);
+	clrb(GROUND_DDR, GROUND_1_PIN);
+	clrb(GROUND_DDR, GROUND_2_PIN);
+	clrb(GROUND_DDR, GROUND_3_PIN);
+}
+
+void ground_get()
+{
+	ground = (GROUND_PORT & 0xF);
+}
